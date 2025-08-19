e6bd527a0f2191f6d5e83a94759ec14c6aae486d
LizardTheWizrd <yassinakkad172@gmail.com>
2025-08-19 11:51:12 +0400

    fix: allowed all origins

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
+
 from routes.notesRoutes import notes_bp  # adjust if your file has a different name
 
 app = Flask(__name__)
 app.register_blueprint(notes_bp)
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
